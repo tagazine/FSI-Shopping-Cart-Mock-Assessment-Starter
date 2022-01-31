5e0b7c4633af2409fe165843e226450f9bc95452
Your Namegit config --global user.name Your <you@example.com>
2022-01-31 13:58:58 -0500

    Change Price

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,14 @@ function changeQuantity(quantDisplay) {
     quantity.innerHTML = quantDisplay
 }
 
+function changePrice(priceDisplay) {
+    let price = document.querySelector('.total-price')
+    price = 15.00 * quantity
+    price.innerHTML = priceDisplay
+}
+
 let quantity = 1
+let price = 0.00
 
 const quantDown = document.querySelector('#quantity-down')
 const quantUp = document.querySelector('#quantity-up')
@@ -14,14 +21,17 @@ quantDown.addEventListener('click', function(e){
         quantity--
     }
     changeQuantity(`Quantity: ${quantity}`)
+    changePrice(`Total Price: ${price}`)
 })
 
 quantUp.addEventListener('click', function(e){
     quantity++
     changeQuantity(`Quantity: ${quantity}`)
+    changePrice(`Total Price: ${price}`)
 })
 
 quantZero.addEventListener('click', function(e){
     quantity = 0
     changeQuantity(`Quantity: ${quantity}`)
+    changePrice(`Total Price: ${price}`)
 })
