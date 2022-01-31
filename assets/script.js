2a60ea77b51742cc8160a195df535358a18b5196
Your Namegit config --global user.name Your <you@example.com>
2022-01-31 09:48:25 -0500

    What?

--- assets/script.js
+++ assets/script.js
@@ -8,14 +8,14 @@ let quantity = 1
 const quantDown = document.querySelector('#quantity-down')
 const quantUp = document.querySelector('#quantity-up')
 
-quantDown.addEventListener('click', function(){
+quantDown.addEventListener('click', function(e){
     if (quantity > 0) {
         quantity--
     }
     changeQuantity('Quantity: ${quantity}')
 })
 
-quantUp.addEventListener('click', function(){
+quantUp.addEventListener('click', function(e){
     quantity++
     changeQuantity('Quantity: ${quantity}')
 })
